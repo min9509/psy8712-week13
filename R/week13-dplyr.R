21ebabf03919abfb1b173ebcb85f89465e48fd9c
minjeong seo <minjeongseo@ra-full-mfa.vpn.umn.edu>
2024-04-27 00:07:07 -0500

    Setting Project

---
+++ R/week13-dplyr.R
@@ -0,0 +1,14 @@
+# Script Settings and Resources
+install.packages("keyring")
+library(keyring)
+install.packages("RMariaDB")
+library(RMariaDB)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+conn <-dbConnect(MariaDB(),
+                 user="seo00082",
+                 password=key_get("latis-mysql","seo00082"),
+                 host="mysql-prod5.oit.umn.edu",
+                 port=3306,
+                 ssl.ca='../mysql_hotel_umn_20220728_interm.cer')
