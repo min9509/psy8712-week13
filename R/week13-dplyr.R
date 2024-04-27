efa48333b5e4a8500a64ab7ce84bbfea69b9f1bc
minjeong seo <minjeongseo@infra04-wg012.eduroam.wireless.umn.edu>
2024-04-27 17:18:43 -0500

    Part 1- Obj 4(1~3)

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -12,14 +12,15 @@ conn <-dbConnect(MariaDB(),
                  host="mysql-prod5.oit.umn.edu",
                  port=3306,
                  ssl.ca='mysql_hotel_umn_20220728_interm.cer')
+# For any procedure that doesn't return a table
 dbExecute(conn, "USE cla_tntlab;")
 
-# Import three datasets 
+# Import three datasets by using dbGetQuery (for any proceduer that returns a table)
 employees_tbl <-  dbGetQuery(conn, "SELECT * FROM cla_tntlab.datascience_employees;")
 testscores_tbl <- dbGetQuery(conn, "SELECT * FROM cla_tntlab.datascience_testscores;") 
 offices_tbl <- dbGetQuery(conn, "SELECT * FROM cla_tntlab.datascience_offices;")
 
-# Export tbls as csv (save in the data file)
+# Export tbls as csv (save in the data file) by using write_csv
 write_csv(employees_tbl, "../data/employees.csv")
 write_csv(testscores_tbl, "../data/testscores.csv")
 write_csv(offices_tbl, "../data/offices.csv")
@@ -32,4 +33,37 @@ week13_tbl <- employees_tbl %>%
 
 # Export tbls as csv (save in the out file)
 write_csv(week13_tbl, "../out/week13.csv")
+
+# Analysis
+# 4-1. the total number of managers (count rows): 549 
+total_managers <- week13_tbl %>%
+  nrow()
+print(total_managers)
+
+# 4_2. the total number of unique managers: 549
+unique_managers <- week13_tbl %>%
+  select(employee_id) %>%
+  unique() %>%
+  nrow()
+print(unique_managers)
+
+# 4-3. A summary of the number of managers split by location
+# city          num_manager
+# 1 Chicago                61
+# 2 Houston                20
+# 3 New York              183
+# 4 Orlando                20
+# 5 San Francisco          48
+# 6 Toronto               189
+
+city_managers <- week13_tbl %>% 
+  #  only include those who were not originally hired as managers
+  filter(manager_hire == "N") %>%
+  # split by location
+  group_by(city) %>%
+  # summary the number of managers
+  summarize(num_manager = n())
+print(city_managers)
+
+# 
 
