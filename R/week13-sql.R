5fe6586d43f7da77ac38851c864cbd15657d3366
minjeong seo <minjeongseo@infra04-wg012.eduroam.wireless.umn.edu>
2024-04-27 18:14:52 -0500

    Part 2- Obj 4(1~3)

--- R/week13-sql.R
+++ R/week13-sql.R
@@ -15,13 +15,39 @@ conn <-dbConnect(MariaDB(),
 dbExecute(conn, "USE cla_tntlab;")
 
 # Analysis
-# 4-1. the total number of managers (count rows): 
+# 4-1. the total number of managers (count rows): 549
+dbGetQuery(conn, 
+  "SELECT COUNT(*) AS total_manager
+   FROM cla_tntlab.datascience_employees AS e
+   INNER JOIN cla_tntlab.datascience_testscores AS t 
+   ON e.employee_id = t.employee_id;"
+)
 
-# 4_2. the total number of unique managers: 
-
+# 4_2. the total number of unique managers: 549
+dbGetQuery(conn, 
+"SELECT COUNT(DISTINCT e.employee_id) AS unique_manager
+FROM cla_tntlab.datascience_employees AS e
+INNER JOIN cla_tntlab.datascience_testscores AS t 
+ON e.employee_id = t.employee_id;"
+)
 
 # 4-3. A summary of the number of managers split by location
+#            city num_managers (Part of results)
+# 1       Chicago           62
+# 2       Houston           21
+# 3      New York          187
+# ... 
 
+dbGetQuery(conn,
+"SELECT city, COUNT(DISTINCT e.employee_id) AS num_managers
+FROM cla_tntlab.datascience_employees AS e
+LEFT JOIN cla_tntlab.datascience_testscores AS t 
+ON e.employee_id = t.employee_id
+LEFT JOIN cla_tntlab.datascience_offices AS o 
+ON city = 'office'
+WHERE manager_hire = 'N'
+GROUP BY city;"
+)
 
 # 4-4. the mean and standard deviation
 
