33b9351d77a39be54473aa56e9e5585f66cbe059
minjeong seo <minjeongseo@infra04-wg012.eduroam.wireless.umn.edu>
2024-04-27 16:42:36 -0500

    Part 1 - Obj 3

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -1,15 +1,27 @@
 # Script Settings and Resources
-install.packages("keyring")
 library(keyring)
-install.packages("RMariaDB")
 library(RMariaDB)
-
+library(tidyverse)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+# Data Import and Cleaning
+# Connect MariaDB
 conn <-dbConnect(MariaDB(),
                  user="seo00082",
                  password=key_get("latis-mysql","seo00082"),
                  host="mysql-prod5.oit.umn.edu",
                  port=3306,
-                 ssl.ca='../mysql_hotel_umn_20220728_interm.cer')
+                 ssl.ca='mysql_hotel_umn_20220728_interm.cer')
+dbExecute(conn, "USE cla_tntlab;")
+
+# Import three datasets 
+employees_tbl <-  dbGetQuery(conn, "SELECT * FROM cla_tntlab.datascience_employees;")
+testscores_tbl <- dbGetQuery(conn, "SELECT * FROM cla_tntlab.datascience_testscores;") 
+offices_tbl <- dbGetQuery(conn, "SELECT * FROM cla_tntlab.datascience_offices;")
+
+# Export tbls as csv
+write_csv(employees_tbl, "../data/employees.csv")
+write_csv(testscores_tbl, "../data/testscores.csv")
+write_csv(offices_tbl, "../data/offices.csv")
+
 
